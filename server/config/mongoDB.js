ff57e57ff648a1ae87666ef247a0323d99b3d08c
Gaurav Khatri <gauravkhatriweb@gmail.com>
2025-07-15 09:45:10 +0500

    feat(database): add MongoDB connection setup
    Initialize MongoDB connection in a separate config file and integrate it into the server startup

---
+++ server/config/mongoDB.js
@@ -0,0 +1,13 @@
+
+import mongoose from 'mongoose';
+
+const conntectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log("MongoDB connection failed", error);
+    }
+};
+
+export default conntectDB;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
-
+import mongoose from 'mongoose';
+import conntectDB from './config/mongoDB.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+conntectDB();
 
 app.use(express.json());
 app.use(cors({credentials:true}));
